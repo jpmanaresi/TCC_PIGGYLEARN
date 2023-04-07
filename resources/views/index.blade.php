43ba3546d424334f5a96731a72b74d3bd2654d66
Felipe <Pipis2x.psilva@gmail.com>
2023-04-06 23:31:53 -0300

    Atualização

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title', 'PL - Inicio')
+    
+@section('content')
+
+<br> <br> <br>
+    
+          <h1 style="text-align: center;">TELA INICIAL</h1>
+    
+          <br> <br>
+
+@endsection
+
+
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,69 +7,67 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="css/app.css">
+        
 
         <title>@yield('title')</title>
+
     </head>
+
+
 <body>
 
 <!-- Navbar -->
-    <nav class="navbar nav-border bg-dark navbar-dark" id="navbar">
-        <div class="container-fluid">
 
-<!-- Sidebar -->
-    <div class="offcanvas offcanvas-start" id="demo">
-        <div class="offcanvas-header">
-        <h1 class="offcanvas-title">Conta</h1>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"></button>
-        </div>
-        <div class="offcanvas-body" id="sidebar">
-        <p>Some text lorem ipsum.</p>
-        <p>Cursos</p>
-        </div>
+<nav class="navbar nav-border" id="nav">
+  <div class="container-fluid">
+
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+      Perfil Menu</button>
+      <!-- Obs: Depois de abrir o offcanvas, se clicar fora o menu continua aberto.
+      Procurar jeito de como fazer com que ele suma ao clicar fora do menu. -->
+
+      <a href="/index" class="navbar-brand">
+        <img src="img/icon-piggy(semcopy).png" width="40px" id="IconePorquinho">
+      </a>
+  </div>
+</nav> 
+      
+<!-- Offcanvas -->
+
+<div class="offcanvas offcanvas-start text-white" data-bs-theme="dark" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" style="color: " id="offcanvasScrollingLabel">
+    Menu</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body " id="sideBar">
+    <div class="d-grid gap-2">
+      <button id="botoesMenus" class="btn btn-custom text-start border-start-0 border-end-0" type="button">Button</button>
+      <button id="botoesMenus" class="btn btn-custom text-start border-start-0 border-end-0" type="button">Button</button>
     </div>
-  
-  <!-- Icone do Offcanvas-->
+  </div>
+</div>
 
-  <a class="navbar-brand" href="#">
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">
-        <span class="navbar-toggler-icon" id="navbar"><img src="img/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24"></span>
-      </button>
-  </a>
-  
-  
- <!-- Barra de pesquisa -->
-    <form class="d-flex d-flex-size rounded-pill" id="navbar">
-        <input type="text" class="form-control m-2 rounded-pill" placeholder="Barra de Pesquisa" style="text-align: center;">
-        <button class="btn btn-tertiary m-2" style="color: violet">
-            <span class="material-symbols-outlined">
-                    Buscar
-            </span>
-        </button>
-    </form>        
+<!-- Conteudo -->
+
               
-              <a href="#" class="navbar-brand">
-                <img src="img/icon-piggy.png" width="60px"/>
-              </a>
-              
-            </div>
-          </nav>
+        
     
-          <br> <br> <br>
-    
-          <h1 style="text-align: center;">TELA INICIAL</h1>
-    
-          <br> <br>
+          
+
+
+<!-- Conteudo da pagina que fica na view welcome -->
 
       @yield('content')
 
+
+
 <!-- Footer -->
+
       <div class="container">
         <footer class="py-3 my-4">
           <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+            
           </ul>
           <p class="text-center text-muted">&copy; 2023 PiggyLearn, Inc</p>
         </footer>
