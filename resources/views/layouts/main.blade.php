6193857b8d01f4def013718331aefbee30223a23
jpmanaresi <jpmanaresi@gmail.com>
2023-04-10 22:26:40 -0300

    troquei a fonte no css e outras parada

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,10 @@
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
       <link rel="stylesheet" href="css/app.css">
-        
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono" rel="stylesheet">
+      
       <title>@yield('title')</title>
 
   </head>
@@ -50,14 +53,22 @@
             Tela Inicial 
           </a>
 @guest
-          <a href="/login" id="botoesMenus" class="btn btn-custom text-start" type="button"> 
+          <a href="/login" id="botoesMenus" class="btn btn-custom text-start" type="button" > 
             Login   
           </a>
 @endguest
 @auth
+  
           <a id="botoesMenus" class="btn btn-custom text-start" type="button"> 
             Cursos  
           </a>
+          <form action="/logout" method="post">
+            @csrf
+              <a href="/login" id="botoesMenus" class="btn btn-custom text-start" type="button" onclick="event.preventDefault();
+          this.closest('form').submit();"> 
+              Sair
+            </a>
+          </form>
 @endauth
         </div>
       </div>
