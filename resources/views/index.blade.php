2e3aac94eff6e8315d957c3464b13b9bbf6e1894
Felipe <Pipis2x.psilva@gmail.com>
2023-04-11 21:40:37 -0300

    Colocando os Cards no index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,70 @@
 @section('content')
 
 
-    <h1 id="tituloP">
+    <h1 id="tituloPagina">
         Tela Inicial
     </h1>
 
+
+    <div class="container">
+        <div class="row">   
+          <div class="col-md-6 col-lg-3 mb-4">
+            <div id="cardbody1" class="card">
+              <div class="card-body">
+                <h5 id="tituloCardIndex" class="card-title">Titulo Generico</h5>
+                <p id="cardtext" class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Donec nibh dolor, gravida ac consequat sit amet, posuere sit amet augue. 
+                    Donec lobortis id nunc et iaculis. Nunc nisl mi, venenatis non tristique eget, lobortis sit amet nulla. 
+                    Maecenas vulputate odio id ipsum sodales convallis. 
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4">
+            <div id="cardbody2" class="card">
+              <div class="card-body">
+                <h5 id="tituloCardIndex" class="card-title">Card 2</h5>
+                <p id="cardtext" class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Donec nibh dolor, gravida ac consequat sit amet, posuere sit amet augue. 
+                    Donec lobortis id nunc et iaculis. Nunc nisl mi, venenatis non tristique eget, lobortis sit amet nulla. 
+                    Maecenas vulputate odio id ipsum sodales convallis. 
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4">
+            <div id="cardbody2" class="card">
+              <div class="card-body">
+                <h5 id="tituloCardIndex" class="card-title">Card 3</h5>
+                <p id="cardtext" class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Donec nibh dolor, gravida ac consequat sit amet, posuere sit amet augue. 
+                    Donec lobortis id nunc et iaculis. Nunc nisl mi, venenatis non tristique eget, lobortis sit amet nulla. 
+                    Maecenas vulputate odio id ipsum sodales convallis. 
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4">
+            <div id="cardbody2" class="card">
+              <div class="card-body">
+                <h5 id="tituloCardIndex" class="card-title">Card 4</h5>
+                <p id="cardtext" class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Donec nibh dolor, gravida ac consequat sit amet, posuere sit amet augue. 
+                    Donec lobortis id nunc et iaculis. Nunc nisl mi, venenatis non tristique eget, lobortis sit amet nulla. 
+                    Maecenas vulputate odio id ipsum sodales convallis. 
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+         
+      
 
 @endsection
 
