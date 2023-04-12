507a969e9f97d6acaa9aab75d0e6aa718598e2ac
Felipe <Pipis2x.psilva@gmail.com>
2023-04-11 22:29:35 -0300

    Cards Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,9 +13,28 @@
     <div class="container">
         <div class="row">   
           <div class="col-md-6 col-lg-3 mb-4">
-            <div id="cardbody1" class="card">
+            <div id="cardbody1" class="card destaque">
               <div class="card-body">
-                <h5 id="tituloCardIndex" class="card-title">Titulo Generico</h5>
+                <h5 id="tituloCardIndex1" class="card-title">
+                  Titulo Generico
+                </h5>
+                <p id="cardtext" class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Donec nibh dolor, gravida ac consequat sit amet, posuere sit amet augue. 
+                    Donec lobortis id nunc et iaculis. Nunc nisl mi, venenatis non tristique eget, lobortis sit amet nulla. 
+                    Maecenas vulputate odio id ipsum sodales convallis. 
+                </p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-md-6 col-lg-3 mb-4">
+            <div id="cardbody2" class="card">
+              <div class="card-body">
+                <h5 id="tituloCardIndex2" class="card-title">
+                  Titulo Foda
+                </h5>
+
                 <p id="cardtext" class="card-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                     Donec nibh dolor, gravida ac consequat sit amet, posuere sit amet augue. 
@@ -28,20 +47,10 @@
           <div class="col-md-6 col-lg-3 mb-4">
             <div id="cardbody2" class="card">
               <div class="card-body">
-                <h5 id="tituloCardIndex" class="card-title">Card 2</h5>
-                <p id="cardtext" class="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Donec nibh dolor, gravida ac consequat sit amet, posuere sit amet augue. 
-                    Donec lobortis id nunc et iaculis. Nunc nisl mi, venenatis non tristique eget, lobortis sit amet nulla. 
-                    Maecenas vulputate odio id ipsum sodales convallis. 
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-3 mb-4">
-            <div id="cardbody2" class="card">
-              <div class="card-body">
-                <h5 id="tituloCardIndex" class="card-title">Card 3</h5>
+                <h5 id="tituloCardIndex2" class="card-title">
+                  Titulo Muito Foda
+                </h5>
+
                 <p id="cardtext" class="card-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                     Donec nibh dolor, gravida ac consequat sit amet, posuere sit amet augue. 
@@ -54,7 +63,10 @@
           <div class="col-md-6 col-lg-3 mb-4">
             <div id="cardbody2" class="card">
               <div class="card-body"> 
-                <h5 id="tituloCardIndex" class="card-title">Card 4</h5>
+                <h5 id="tituloCardIndex2" class="card-title">
+                  Vai se Lascar vai
+                </h5>
+
                 <p id="cardtext" class="card-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                     Donec nibh dolor, gravida ac consequat sit amet, posuere sit amet augue. 
