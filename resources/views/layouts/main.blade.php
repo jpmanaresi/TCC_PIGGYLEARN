aad99007e47717a78c08cedff2cd1a81133a4d1b
Felipe <Pipis2x.psilva@gmail.com>
2023-04-13 20:29:14 -0300

    Teste

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,7 +31,7 @@
           </div>
         </a>
       
-        <a href="/" class="navbar-brand" id="offCanvasButton">
+        <a href="/" class="navbar-brand">
           <img width="40px" src="img/icon-piggy(semcopy).png" alt="IconePorquinho">
         </a>
 
