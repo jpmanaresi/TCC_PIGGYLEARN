250315603180f983cf10b571f5ab89284af8e342
Felipe <Pipis2x.psilva@gmail.com>
2023-04-13 21:30:29 -0300

    teste

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,7 +31,7 @@
           </div>
         </a>
       
-        <a href="/" class="justify-content-end"">
+        <a href="/" class="justify-content-end">
           <img width="40px" src="img/icon-piggy(semcopy).png" alt="IconePorquinho">
         </a>
 
