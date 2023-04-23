192b62fbd8c09a70aa3f41269fa46845b9dface9
GeoBarretto <barrettogeo26@gmail.com>
2023-04-22 22:35:58 -0300

    gh

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -14,6 +14,7 @@
             <img id="imagemperfil" src="/img/User-60.svg" alt="">
             <span class="input-group-addon  justify-content-md-center">
                 <button class="btn btn-secondary me-md-2" id="iconlapis">
+                    <input name="myFile" type="file">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                       </svg>
@@ -21,7 +22,7 @@
               </span>
             </div>
             <br> <br>
-
+            
         <form>
             <h1 id="tituloProfileI">
                 Nome de Usuário
@@ -36,6 +37,33 @@
     <Br>
 
     <form>
+
+     <!-- NOMES -->
+     <div class="row">
+        <div class="col justify-content-around">         
+              <label for="email" class="form-label">
+                 <p id="p-label">
+                     Nome
+                 </p>
+              </label>
+
+              <div id="displayflex"> <!-- div para colocar os elementos um ao lado do outro -->  
+                <input type="text" aria-label="First name" placeholder="Nome completo" class="form-control" id="exampleInput1">
+
+              <span class="input-group-addon d-grid gap-2 d-md-flex justify-content-md-end">
+                 <button class="btn btn-secondary me-md-2" id="iconlapis">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                       </svg>
+                 </button>
+               </span>
+
+              </div>    
+       </div>
+      <div class="col">
+          
+      </div>
+     </div>
      <div class="row">
            <div class="col justify-content-around">    
             <!-- texto email -->        
@@ -46,7 +74,7 @@
                  </label>
 
                  <div id="displayflex"> <!-- div para colocar os elementos um ao lado do outro -->  
-                 <input type="email" class="form-control" id="exampleInput1" aria-describedby="emailHelp" > 
+                 <input type="email" class="form-control" id="exampleInput1" aria-describedby="emailHelp" placeholder="exemplo@exemplo.com" > 
 
                  <span class="input-group-addon d-grid gap-2 d-md-flex justify-content-md-end">
                     <button class="btn btn-secondary me-md-2" id="iconlapis">
@@ -58,18 +86,20 @@
 
                  </div>
 
-                 <div id="emailHelp" class="form-text">Nós nunca compartilharemos seu email com ninguém</div>        
+                       
           </div>
          <div class="col justify-content-around">
             <label for="email" class="form-label">
                 <p id="p-label">
-                    Email
+                    Telefone
                 </p>
              </label>
-
-             <div id="displayflex"> <!-- div para colocar os elementos um ao lado do outro -->  
-             <input type="email" class="form-control" id="exampleInput1" aria-describedby="emailHelp" > 
-
+             <div id="displayflex"> <!-- div para colocar os elementos um ao lado do outro --> 
+             <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">+55</span>
+                <input type="text" class="form-control" id="exampleInput2" placeholder="(xx) x xxxx-xxxx" aria-label="Username" aria-describedby="basic-addon1">
+              
+            
              <span class="input-group-addon d-grid gap-2 d-md-flex justify-content-md-end">
                 <button class="btn btn-secondary me-md-2" id="iconlapis">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
@@ -77,36 +107,12 @@
                       </svg>
                 </button>
               </span>
+            </div>
          </div>
         </div>
 
 
- <!-- NOMES -->
-        <div class="row">
-            <div class="col justify-content-around">         
-                  <label for="email" class="form-label">
-                     <p id="p-label">
-                         Nome
-                     </p>
-                  </label>
- 
-                  <div id="displayflex"> <!-- div para colocar os elementos um ao lado do outro -->  
-                    <input type="text" aria-label="First name" placeholder="Nome completo" class="form-control" id="exampleInput1">
- 
-                  <span class="input-group-addon d-grid gap-2 d-md-flex justify-content-md-end">
-                     <button class="btn btn-secondary me-md-2" id="iconlapis">
-                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                             <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                           </svg>
-                     </button>
-                   </span>
- 
-                  </div>    
-           </div>
-          <div class="col">
-              
-          </div>
-         </div>
+
     </form>
 </div>
 
