0bdb956bc41879c0031813d85bf3f0a3c739117c
GeoBarretto <barrettogeo26@gmail.com>
2023-04-27 22:08:48 -0300

    .

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -3,14 +3,24 @@
 @section('title', $course->title ) 
 
 @section('content')
-<div class="col-md-10 offset-md-1">
+
+
+<div class="container-md" id="showborda">
 <div class="row">
-    <div id="info-container" class="col-md-6">
-        <h1>{{$course->course_title}}</h1>
+    <div id="info-container" class="col">
+        
+        <h1 id="tituloCurso">{{$course->course_title}}</h1>
+
     </div>
+
+    <span id="regua"></span>
+
     <div class="col-md-12" id="description-container">
-        <h3>Sobre o curso:</h3>
-        <p class="event-description">{{$course->course_description}}</p>
+
+        <h2 id="h2-show1">Sobre o curso:</h2>
+
+        <p id="p-show1" class="event-description">{{$course->course_description}}</p>
+        
     </div>
 </div>
 
