113f817c3114125f1f06a0c390553942f8d8fea7
jpmanaresi <jpmanaresi@gmail.com>
2023-04-30 10:37:08 -0300

    .

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -3,17 +3,19 @@
 @section('title', $course->title ) 
 
 @section('content')
-<div class="col-md-10 offset-md-1">
-<div class="row">
-    <div id="info-container" class="col-md-6">
-        <h1>{{$course->course_title}}</h1>
-    </div>
-    <div class="col-md-12" id="description-container">
-        <h3>Sobre o curso:</h3>
-        <p class="event-description">{{$course->course_description}}</p>
-    </div>
-</div>
 
+<div class="container" id="course-show-container">
+    <div class="col-md-10 offset-md-1">
+        <div class="row">
+            <div id="info-container" class="col-md-6">
+                <h1>{{$course->course_title}}</h1>
+            </div>
+            <div class="col-md-12" id="description-container">
+                <h3>Sobre o curso:</h3>
+                <p class="event-description">{{$course->course_description}}</p>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
 
