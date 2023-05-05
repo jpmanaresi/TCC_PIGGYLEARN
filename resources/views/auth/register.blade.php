f91418ced3cc428df41c0e70f1205d98893f59d3
GeoBarretto <barrettogeo26@gmail.com>
2023-05-05 20:18:15 -0300

    .

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card" id="cardregister">
                 <div class="card-header" id="tituloregister">{{ __('Register') }}</div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
