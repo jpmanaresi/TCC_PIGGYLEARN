750478150fca5d8681158a528e5b44e2e5aaf026
jpmanaresi <jpmanaresi@gmail.com>
2023-06-01 21:10:24 -0300

    .

---
+++ database/migrations/2023_06_01_225435_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->text('question');
+            $table->text('alt_1');
+            $table->text('alt_2');
+            $table->text('alt_3');
+            $table->text('alt_4');
+            $table->string('answer');
+            $table->foreignId('test_id')->constrained('tests')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
