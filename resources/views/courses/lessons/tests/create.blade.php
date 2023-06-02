3d255d7760465d079d1e4d9c9f84b370fb79a91c
jpmanaresi <jpmanaresi@gmail.com>
2023-06-01 21:10:13 -0300

    .

--- resources/views/courses/lessons/tests/create.blade.php
+++ resources/views/courses/lessons/tests/create.blade.php
@@ -5,19 +5,26 @@
 @section('content')
 
 <div class="container" id="lesson-form-container">
-    <form method="POST" action="{{route('tests.store')}}">
+    <form method="POST" action="{{ isset($test) ? route('tests.update', $test->id) : route('tests.store') }}">
         @csrf
-        <input type="hidden" name="lesson_id" value="{{$lesson->id}}">
+
+        @if(isset($test))
+            @method('PUT')
+        @endif
+
+        <input type="hidden" name="test_id" value="{{ isset($test) ? $test->id : '' }}">
+        
         <div class="form-group">
-            <label for="title" >Aula:</label>
-            <input type="text" id="title" class="form-control"name="title" value="Avaliação: Aula {{$lesson->seq}}" disabled>
+            <label for="title">Aula:</label>
+            <input type="text" id="title" class="form-control" name="title" value="{{ isset($test) ? 'Avaliação: Aula ' . $lesson->seq : '' }}" disabled>
         </div>
+        
         <div class="form-group">
-            <label for="title" >Descrição:</label>
-            <textarea name="description" id="content" class="form-control" placeholder="Descrição"></textarea>
+            <label for="description">Descrição:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="Descrição">{{ isset($test) ? $test->description : '' }}</textarea>
         </div>
 
-        <button type="submit">Criar Prova</button>
+        <button type="submit">{{ isset($test) ? 'Atualizar Prova' : 'Criar Prova' }}</button>
     </form>
 </div>
 @endsection
