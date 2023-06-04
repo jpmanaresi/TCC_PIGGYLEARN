bd7e193a5fa58c85abc7bee5cc3b55e8c9c76112
FelipinhoLegal <pipis2x.psilva@gmail.com>
2023-06-04 17:32:51 -0300

    Funciona Porfavor

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -38,5 +37,4 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-    
 }
--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,7 @@ Route::get('/dashboard', [CourseController::class, 'dashboard'])->name('dashboar
 Auth::routes();
 
 /*Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
