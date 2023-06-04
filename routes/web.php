2419668fd8c705606db2c167b3572fe24285ee3c
FelipinhoLegal <pipis2x.psilva@gmail.com>
2023-06-04 18:30:27 -0300

    Merda resolvida?

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,3 @@ Route::get('/dashboard', [CourseController::class, 'dashboard'])->name('dashboar
 Auth::routes();
 
 /*Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
