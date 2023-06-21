2b35cf18ba412dfaec8760a5886240aba410a2b0
FelipinhoLegal <pipis2x.psilva@gmail.com>
2023-06-21 16:22:03 -0300

    Comi te

--- resources/views/courses/lessons/tests/questions/show.blade.php
+++ resources/views/courses/lessons/tests/questions/show.blade.php
@@ -1,6 +1,6 @@
 @extends ('layouts.main')
 
-@section('title', $question->title ) 
+@section('title', $question->title) 
 
 @section('content')
 
@@ -15,50 +15,47 @@
                 <div id="descricaoMostrarT" class="col-md-12" id="description-container">
                     <p id="descricaoMostrarT" class="event-description">{{$question->question}}</h1>
                 </div>
-                
 
                 <div id="corpoTabela" class="col-md-12">
-                    <form action="/validate-answer" method="POST" id="validate-question">
-                        @csrf
-                        <meta name="csrf-token" content="{{ csrf_token() }}">
-                        <input type="hidden" name="questionId" value="{{$question->id}}">
-                
-                        <div class="row justify-content-center">
-                            <div style="margin-left: 4px;" class="col-auto d-flex align-items-center">
-                                <input type="radio" name="answer" id="answer_1" value="alt_1" data-question-id="{{$question->id}}">
+                    <div class="form-group">
+                        <form action="/validate-answer" method="POST" id="validate-question">
+                            @csrf
+                            <meta name="csrf-token" content="{{ csrf_token() }}">
+                            <input type="hidden" name="questionId" value="{{$question->id}}">
+                    
+                            <div class="row justify-content-center">
+                                <div style="margin-left: 4px;" class="col-auto d-flex align-items-center">
+                                    <input type="radio" name="answer" id="answer_1" value="alt_1" data-question-id="{{$question->id}}">
+
+                                    <p id="textoRespostaQ" class="event-description">{{$question->alt_1}}</p>
+                                </div>
                             </div>
-                            <div class="col d-flex align-items-center">
-                                <p id="textoRespostaQ" class="event-description">{{$question->alt_1}}</p>
+                    
+                            <div class="row justify-content-center">
+                                <div style="margin-left: 4px;" value="alt_2" class="col-auto d-flex align-items-center">
+                                    <input type="radio" name="answer" id="answer_2"  data-question-id="{{$question->id}}">
+                                
+                                    <p id="textoRespostaQ" class="event-description">{{$question->alt_2}}</p>
+                                </div>
                             </div>
-                        </div>
-                
-                        <div class="row justify-content-center">
-                            <div style="margin-left: 4px;" class="col-auto d-flex align-items-center">
-                                <input type="radio" name="answer" id="answer_2" value="alt_2" data-question-id="{{$question->id}}">
+                    
+                            <div class="row justify-content-center">
+                                <div style="margin-left: 4px;" class="col-auto d-flex align-items-center">
+                                    <input type="radio" name="answer" id="answer_3" value="alt_3" data-question-id="{{$question->id}}">
+                               
+                                    <p id="textoRespostaQ" class="event-description">{{$question->alt_3}}</p>
+                                </div>
                             </div>
-                            <div class="col d-flex align-items-center">
-                                <p id="textoRespostaQ" class="event-description">{{$question->alt_2}}</p>
+                    
+                            <div class="row justify-content-center">
+                                <div style="margin-left: 4px;" class="col-auto d-flex align-items-center">
+                                    <input type="radio" name="answer" id="answer_4" value="alt_4" data-question-id="{{$question->id}}">
+                               
+                                    <p id="textoRespostaQ" class="event-description">{{$question->alt_4}}</p>
+                                </div>
                             </div>
-                        </div>
-                
-                        <div class="row justify-content-center">
-                            <div style="margin-left: 4px;" class="col-auto d-flex align-items-center">
-                                <input type="radio" name="answer" id="answer_3" value="alt_3" data-question-id="{{$question->id}}">
-                            </div>
-                            <div class="col d-flex align-items-center">
-                                <p id="textoRespostaQ" class="event-description">{{$question->alt_3}}</p>
-                            </div>
-                        </div>
-                
-                        <div class="row justify-content-center">
-                            <div style="margin-left: 4px;" class="col-auto d-flex align-items-center">
-                                <input type="radio" name="answer" id="answer_4" value="alt_4" data-question-id="{{$question->id}}">
-                            </div>
-                            <div class="col d-flex align-items-center">
-                                <p id="textoRespostaQ" class="event-description">{{$question->alt_4}}</p>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
                 
                 @csrf
