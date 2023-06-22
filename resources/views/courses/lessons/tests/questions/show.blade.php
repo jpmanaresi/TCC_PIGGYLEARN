312cc42a4d394a141b1677e8d0b7b381e8efc5f0
jpmanaresi <jpmanaresi@gmail.com>
2023-06-22 20:17:27 -0300

    .

--- resources/views/courses/lessons/tests/questions/show.blade.php
+++ resources/views/courses/lessons/tests/questions/show.blade.php
@@ -73,7 +73,7 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="{{ asset('/js/validarResposta.js') }}"></script>
+    <script src="/js/validarResposta.js'"></script>
 
 
 @endsection
