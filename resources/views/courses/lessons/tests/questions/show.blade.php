a944643582fa43201da1b27d54a3f368e2461fd3
jpmanaresi <jpmanaresi@gmail.com>
2023-06-23 09:26:00 -0300

    .

--- resources/views/courses/lessons/tests/questions/show.blade.php
+++ resources/views/courses/lessons/tests/questions/show.blade.php
@@ -71,7 +71,7 @@
             </div>
         </div>
     </div>
-    <script src="{{ secure_asset('/js/scripts/validarResposta.js') }}"></script>
+    <script src="{{ asset('/js/scripts/validarResposta.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 
